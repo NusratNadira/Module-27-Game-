4a7d3b0f50ba80545daf1da2399b71d468a35919
NusratNadira <nusratnadira2@gmail.com>
2024-02-15 14:04:49 +0600

    upload

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ function showElements(elementId){
     const element = document.getElementById(elementId);
     element.classList.add('bg-orange-400');
  } 
+ function removebackgroundColor(elementId){
+   const element = document.getElementById(elementId);
+   element.classList.remove('bg-orange-400');
+} 
     
     function getRandomAlfa(){
         const alfastring ='abcdefghijklmnopqrstuvwxyz';
@@ -28,7 +32,13 @@ function showElements(elementId){
 
 function playNow(){
    hideElements('home-screen');
+   hideElements('final-score');
 showElements('play-ground');
+
+//reset score and life
+setTextElementValueById('current-life', 5);
+setTextElementValueById('current-score', 0);
+
 continueGame() ;
 }
 
@@ -36,4 +46,86 @@ function continueGame(){
 const alfa = getRandomAlfa();
 document.getElementById('current-alphabet').innerText= alfa;
 setbackgroundColor(alfa);
+}
+
+
+
+
+function handleKeyboardKeyUpEvent(event){
+   //get the pressed alphabet
+const playerPressed = event.key;
+//stop the game
+if(playerPressed==='Escape'){
+   gameOver();
+}
+
+//get the screen's alphabet in lower case
+const currentAlphabetElement = document.getElementById('current-alphabet').innerText.toLowerCase();
+
+
+//check matched or not
+if(playerPressed === currentAlphabetElement){
+   console.log('You got a point');
+   removebackgroundColor(currentAlphabetElement);
+   continueGame();
+   //update score:
+   // 1.get the current score
+   const currentScore = getTextElementValueById('current-score');
+   // 2. increase the score by 1
+   const newScore = currentScore + 1;
+   // 3. show the updated score
+  setTextElementValueById('current-score', newScore );
+
+  
+}
+else{
+   //step1-get the current life Number
+const life=getTextElementValueById('current-life');
+   //step2- reduce the life count
+   const newlife = life - 1;
+   //step3- display the duplicated life count
+  setTextElementValueById('current-life' , newlife)
+
+if(newlife === 0){
+  gameOver();
+ 
+}
+
+}
+}
+
+document.addEventListener('keyup',handleKeyboardKeyUpEvent);
+
+
+
+function gameOver(){
+   hideElements('play-ground');
+   showElements('final-score');
+   //update final score
+   //1. get the final Score
+const lastScore= getTextElementValueById('current-score');
+
+setTextElementValueById('game-score', lastScore);
+
+//clear the last selected key
+  const currentAlphabet = getElement('current-alphabet');
+  removebackgroundColor(currentAlphabet);
+}
+
+
+function getTextElementValueById(elementId){
+const element = document.getElementById(elementId).innerText;
+const value= parseInt(element);
+return value;
+}
+
+function setTextElementValueById(elementId , value){
+const element = document.getElementById(elementId);
+ element.innerText=value;
+}
+
+function getElement(elementId){
+   const element = document.getElementById(elementId);
+   const value = element.innerText;
+   return value;
 }
